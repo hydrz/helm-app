7ec86cba351b7009e4352c606c5e7e8100d4edb9
denghy <denghy@fxft.net>
2019-11-18 15:05:32 +0800

    stable

--- helm-app.sh
+++ helm-app.sh
@@ -2,7 +2,7 @@
 set -e
 
 # nfs-client-provisioner
-helm upgrade ./nfs-client-provisioner \
+helm upgrade stable/nfs-client-provisioner \
     --name=nfs --namespace=kube-system \
     --set image.repository=quay-mirror.qiniu.com/external_storage/nfs-client-provisioner \
     --set nfs.server=x.x.x.x \
